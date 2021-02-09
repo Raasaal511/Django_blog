0db673af65e1fde934cb68044adc52f2da249830
Rasul <raasaal511@gmail.com>
2021-02-09 11:47:54 +0300

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+	"""Создает пост с пользователем из settings"""
 	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	title = models.CharField(max_length=100)
 	text = models.TextField()
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	"""Получает данные из базы данных и выводит и на гланный страницу"""
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	
+	return render(request, 'blog/post_list.html', {'posts': posts})
